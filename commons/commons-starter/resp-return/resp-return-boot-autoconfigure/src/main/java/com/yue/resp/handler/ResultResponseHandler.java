710c6e8f8f4c701dfda34b17f9748ec3837c23f6
Your Name <2422669375@qq.com>
2023-05-31 01:53:20 +0800

    组件更新

--- commons/commons-starter/resp-return/resp-return-boot-autoconfigure/src/main/java/com/yue/resp/handler/ResultResponseHandler.java
+++ commons/commons-starter/resp-return/resp-return-boot-autoconfigure/src/main/java/com/yue/resp/handler/ResultResponseHandler.java
@@ -25,7 +25,7 @@ public class ResultResponseHandler implements ResponseBodyAdvice<Object> {
     @Override
     public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
         if(body instanceof String s){
-            return JSON.toJSONString(R.ok(body));
+            return JSON.toJSONString(R.ok().message(s));
         }else if(body instanceof Error error){
             return R.error().message(error.getMessage()).code(error.getCode());
         }else if(body instanceof R){
